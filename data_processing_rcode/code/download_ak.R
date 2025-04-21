ccd64f35aae807d65c3343982a27ff4012b042de
Em Markowitz (NOAA) <75965120+EmilyMarkowitz-NOAA@users.noreply.github.com>
2025-04-20 20:47:53 -0700

    Update download_ak.R
    minor edits

--- data_processing_rcode/code/download_ak.R
+++ data_processing_rcode/code/download_ak.R
@@ -93,66 +93,11 @@ for (i in seq(0, 1000000, 10000)){
 }
 catch <- unique(dat)
 
-############ Testing # As of April 21, 2025
+# mostly for testing, but also nice to have it organized
+catch <- catch[order(catch$species_code), ]
+catch <- catch[order(catch$hauljoin), ]
 
-dim(catch)
-# 438608      7
-summary(catch)
-# hauljoin        species_code     cpue_kgkm2        cpue_nokm2          count           weight_kg         taxon_confidence  
-# Min.   : -23911   Min.   :    1   Min.   :      0   Min.   :     14   Min.   :    1.0   Min.   :    0.001   Length:438608     
-# 1st Qu.: -14310   1st Qu.:10130   1st Qu.:     25   1st Qu.:     64   1st Qu.:    2.0   1st Qu.:    0.872   Class :character  
-# Median :  -4750   Median :20510   Median :    171   Median :    243   Median :    9.0   Median :    5.900   Mode  :character  
-# Mean   : 284057   Mean   :16337   Mean   :   2236   Mean   :   4437   Mean   :  154.4   Mean   :   71.638                     
-# 3rd Qu.: 802455   3rd Qu.:21740   3rd Qu.:    897   3rd Qu.:   1307   3rd Qu.:   46.0   3rd Qu.:   31.320                     
-# Max.   :1225635   Max.   :30600   Max.   :3226235   Max.   :4481702   Max.   :47118.0   Max.   :18187.700                     
-#                                   NA's   :943       NA's   :943                 
-
-### Download Catch Data - Alt --------------------------------------------------
-# pull all data and crop to the species codes less than 32000
-
-dat <- data.frame()
-  for (i in seq(0, 1000000, 10000)){
-    ## find how many iterations it takes to cycle through the data
-    print(i)
-    ## query the API link
-    res <- httr::GET(url = paste0("https://apps-st.fisheries.noaa.gov/ods/foss/afsc_groundfish_survey_catch/",
-                                  "?offset=",i,"&limit=10000")) # '&q={"species_code":{"$lt":32000}}'
-    ## convert from JSON format
-    data <- jsonlite::fromJSON(base::rawToChar(res$content))
-
-    ## if there are no data, stop the loop
-    if (is.null(nrow(data$items))) {
-      break
-    }
-
-    ## bind sub-pull to dat data.frame
-    dat <- dplyr::bind_rows(dat,
-                            data$items %>%
-                              dplyr::select(-links)) # necessary for API accounting, but not part of the dataset)
-  }
-
-
-
-catch <- dat %>%
-  dplyr::filter(species_code < 32000) %>%
-  unique()
-
-############ Testing # As of April 21, 2025
-# all data
-dim(dat)
-# 891144      7
-summary(dat)
-# hauljoin        species_code     cpue_kgkm2        cpue_nokm2           count            weight_kg         taxon_confidence
-# Min.   : -23911   Min.   :    1   Min.   :      0   Min.   :      13   Min.   :     1.0   Min.   :    0.001   Length:891144
-# 1st Qu.: -14439   1st Qu.:20510   1st Qu.:      6   1st Qu.:      58   1st Qu.:     2.0   1st Qu.:    0.199   Class :character
-# Median :  -5267   Median :40500   Median :     49   Median :     214   Median :     8.0   Median :    1.814   Mode  :character
-# Mean   : 280338   Mean   :45195   Mean   :   1250   Mean   :    4605   Mean   :   180.5   Mean   :   41.720
-# 3rd Qu.: 802426   3rd Qu.:71800   3rd Qu.:    372   3rd Qu.:    1137   3rd Qu.:    43.0   3rd Qu.:   13.780
-# Max.   :1225635   Max.   :99999   Max.   :3226235   Max.   :21780780   Max.   :867119.0   Max.   :18187.700
-#                                   NA's   :87811      NA's   :87811
-
-############ Testing
-# data cropped to species codes of interest
+############ Testing # April 21, 2025
 dim(catch)
 # 438608      7
 summary(catch)
@@ -165,12 +110,70 @@ summary(catch)
 # Max.   :1225635   Max.   :30600   Max.   :3226235   Max.   :4481702   Max.   :47118.0   Max.   :18187.700
 #                                   NA's   :943       NA's   :943
 
-# mostly for testing, but also nice to have it organized
-catch <- catch[order(catch$species_code), ]
-catch <- catch[order(catch$hauljoin), ]
-
-write.csv(x = catch,
-          here::here("data_processing_rcode/data/AK_gap_products_foss_catch.csv"))
+# ### Download Catch Data - Alt --------------------------------------------------
+# # pull all data and crop to the species codes less than 32000
+#
+# dat <- data.frame()
+#   for (i in seq(0, 1000000, 10000)){
+#     ## find how many iterations it takes to cycle through the data
+#     print(i)
+#     ## query the API link
+#     res <- httr::GET(url = paste0("https://apps-st.fisheries.noaa.gov/ods/foss/afsc_groundfish_survey_catch/",
+#                                   "?offset=",i,"&limit=10000")) # '&q={"species_code":{"$lt":32000}}'
+#     ## convert from JSON format
+#     data <- jsonlite::fromJSON(base::rawToChar(res$content))
+#
+#     ## if there are no data, stop the loop
+#     if (is.null(nrow(data$items))) {
+#       break
+#     }
+#
+#     ## bind sub-pull to dat data.frame
+#     dat <- dplyr::bind_rows(dat,
+#                             data$items %>%
+#                               dplyr::select(-links)) # necessary for API accounting, but not part of the dataset)
+#   }
+#
+#
+#
+# catch <- dat %>%
+#   dplyr::filter(species_code < 32000) %>%
+#   unique()
+#
+# ############ Testing # April 21, 2025
+# # all data
+# dim(dat)
+# # 891144      7
+# summary(dat)
+# # hauljoin        species_code     cpue_kgkm2        cpue_nokm2           count            weight_kg         taxon_confidence
+# # Min.   : -23911   Min.   :    1   Min.   :      0   Min.   :      13   Min.   :     1.0   Min.   :    0.001   Length:891144
+# # 1st Qu.: -14439   1st Qu.:20510   1st Qu.:      6   1st Qu.:      58   1st Qu.:     2.0   1st Qu.:    0.199   Class :character
+# # Median :  -5267   Median :40500   Median :     49   Median :     214   Median :     8.0   Median :    1.814   Mode  :character
+# # Mean   : 280338   Mean   :45195   Mean   :   1250   Mean   :    4605   Mean   :   180.5   Mean   :   41.720
+# # 3rd Qu.: 802426   3rd Qu.:71800   3rd Qu.:    372   3rd Qu.:    1137   3rd Qu.:    43.0   3rd Qu.:   13.780
+# # Max.   :1225635   Max.   :99999   Max.   :3226235   Max.   :21780780   Max.   :867119.0   Max.   :18187.700
+# #                                   NA's   :87811      NA's   :87811
+#
+# ############ Testing # April 21, 2025
+# # data cropped to species codes of interest
+# dim(catch)
+# # 438608      7
+# summary(catch)
+# # hauljoin        species_code     cpue_kgkm2        cpue_nokm2          count           weight_kg         taxon_confidence
+# # Min.   : -23911   Min.   :    1   Min.   :      0   Min.   :     14   Min.   :    1.0   Min.   :    0.001   Length:438608
+# # 1st Qu.: -14310   1st Qu.:10130   1st Qu.:     25   1st Qu.:     64   1st Qu.:    2.0   1st Qu.:    0.872   Class :character
+# # Median :  -4750   Median :20510   Median :    171   Median :    243   Median :    9.0   Median :    5.900   Mode  :character
+# # Mean   : 284057   Mean   :16337   Mean   :   2236   Mean   :   4437   Mean   :  154.4   Mean   :   71.638
+# # 3rd Qu.: 802455   3rd Qu.:21740   3rd Qu.:    897   3rd Qu.:   1307   3rd Qu.:   46.0   3rd Qu.:   31.320
+# # Max.   :1225635   Max.   :30600   Max.   :3226235   Max.   :4481702   Max.   :47118.0   Max.   :18187.700
+# #                                   NA's   :943       NA's   :943
+#
+# # mostly for testing, but also nice to have it organized
+# catch <- catch[order(catch$species_code), ]
+# catch <- catch[order(catch$hauljoin), ]
+#
+# write.csv(x = catch,
+#           here::here("data_processing_rcode/data/AK_gap_products_foss_catch.csv"))
 
 # # Zero-Filled Data -----------------------------------------------------------
 #
