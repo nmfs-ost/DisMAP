c39f474db73c705b3ae1aa6bd2c283696906ceab
ClaireGonzales-NOAA <claire.gonzales@noaa.gov>
2025-04-23 17:13:05 -0400

    code built for percentile wtcpue

--- data_processing_rcode/code/species_persistence.R
+++ data_processing_rcode/code/species_persistence.R
@@ -20,43 +20,74 @@ library(RColorBrewer)
 # Read in data from Compile_Dismap_Current.r
 ## Note: I am using example data from last year for now##
 data_ex <- data.frame(base::readRDS(file = here::here("data_processing_rcode/output/data_clean/alldata_withzeros.rds")))
-mydata_filt <- data_ex %>%
-  filter(region == "Aleutian Islands" & spp == "Gadus macrocephalus")
 
-############ PAUSED HERE - finish writing this conditional ######################
-# data__update <- data_ex %>%
-#   if(region == "Aleutian Islands") {
-#     mutate(survey = "Aleutian Islands Bottom Trawl Survey")
-#   } else if (region == "Eastern Bering Sea") {
-#       mutate(survey = "Eastern Bering Sea Crab/Groundfish Bottom Trawl Survey")
-#     } else if
+# This version does not include data from Hawai'i Islands, but this should be added in
+data_ex$survey <- ifelse(data_ex$region == "Aleutian Islands", "Aleutian Islands Bottom Trawl Survey",
+                         ifelse(data_ex$region == "Eastern Bering Sea", "Eastern Bering Sea Crab/Groundfish Bottom Trawl Survey",
+                                ifelse(data_ex$region == "Northern Bering Sea", "Northern Bering Sea Crab/Groundfish Survey - Eastern Bering Sea Shelf Survey Extension",
+                                       ifelse(data_ex$region == "Gulf of Alaska", "Gulf of Alaska Bottom Trawl Survey",
+                                              ifelse(data_ex$region == "Gulf of Mexico", "Gulf of Mexico Summer Shrimp/Groundfish Survey",
+                                                     ifelse(data_ex$region == "Northeast US Fall", "NEFSC Fall Bottom Trawl",
+                                                            ifelse(data_ex$region == "Northeast US Spring", "NEFSC Spring Bottom Trawl",
+                                                                   ifelse(data_ex$region == "Southeast US Fall", "SEAMAP Fall Coastal Trawl Survey",
+                                                                          ifelse(data_ex$region == "Southeast US Spring", "SEAMAP Spring Coastal Trawl Survey",
+                                                                                 ifelse(data_ex$region == "Southeast US Summer", "SEAMAP Summer Coastal Trawl Survey",
+                                                                                        ifelse(data_ex$region == "West Coast Annual", "West Coast Bottom Trawl Annual",
+                                                                                               ifelse(data_ex$region == "West Coast Triennial", "West Coast Bottom Trawl Triennial",
+                                                                                                      ifelse(data_ex$region == "Eastern and Northern Bering Sea", NA, NA)))))))))))))
 
-data_wide <- data_ex %>%
-  # group_by(region, year, spp) %>%
-  # summarise(wtcpue = sum(wtcpue)) %>%
-  select(-c(haulid, stratum, stratumarea, lat, lon, depth))
-  pivot_wider(names_from = year, values_from = wtcpue, names_prefix = "yr") %>%
-  mutate(
-    yrs1993_90 = yr1993 - yr1990,
-    yrs1996_93 = yr1996 - yr1993,
-    yrs1999_96 = yr1999 - yr1996,
-    yrs2003_99 = yr2003 - yr1999,
-    yrs2005_03 = yr2005 - yr2003,
-    yrs2007_05 = yr2007 - yr2005,
-    yrs2009_07 = yr2009 - yr2007,
-    yrs2011_09 = yr2011 - yr2009,
-    yrs2013_11 = yr2013 - yr2011,
-    yrs2015_13 = yr2015 - yr2013,
-    yrs2017_15 = yr2017 - yr2015,
-    yrs2019_17 = yr2019 - yr2017,
-    yrs2021_19 = yr2021 - yr2019,
-    yrs2023_21 = yr2023 - yr2021
-  ) %>%
-  ungroup() %>%
-  select(region, spp, yrs1993_90:yrs2023_21) %>%
-  mutate(total_change_wtcpue = yrs1993_90 + yrs1996_93 + yrs1999_96 + yrs2003_99 + yrs2005_03 + yrs2007_05 +
-                                   yrs2009_07 + yrs2011_09 + yrs2013_11 + yrs2015_13 + yrs2017_15 + yrs2019_17 +
-                                   yrs2021_19 + yrs2023_21) # this is a separate table
+data_update <- data_ex %>%
+  filter(!is.na(survey)) %>%
+  mutate(region_old = region) %>%
+  select(-region)
+
+data_update$region <- ifelse(data_update$region_old == "Aleutian Islands", "Aleutian Islands",
+                             ifelse(data_update$region_old == "Eastern Bering Sea", "Eastern Bering Sea",
+                                    ifelse(data_update$region_old == "Northern Bering Sea", "Northern Bering Sea",
+                                           ifelse(data_update$region_old == "Gulf of Alaska", "Gulf of Alaska",
+                                                  ifelse(data_update$region_old == "Gulf of Mexico", "Gulf of Mexico",
+                                                         ifelse(data_update$region_old == "Northeast US Fall", "Northeast US",
+                                                                ifelse(data_update$region_old == "Northeast US Spring", "Northeast US",
+                                                                       ifelse(data_update$region_old == "Southeast US Fall", "Southeast US",
+                                                                              ifelse(data_update$region_old == "Southeast US Spring", "Southeast US",
+                                                                                     ifelse(data_update$region_old == "Southeast US Summer", "Southeast US",
+                                                                                            ifelse(data_update$region_old == "West Coast Annual", "West Coast",
+                                                                                                   ifelse(data_update$region_old == "West Coast Triennial", "West Coast", NA))))))))))))
+
+
+# caculate percentile for each
+data_rank <- data_update %>%
+  select(region, survey, year, spp, common, wtcpue) %>%
+  group_by(region, survey, year, spp) %>%
+  summarise(wtcpue = sum(wtcpue)) %>%
+  group_by(region, survey, spp) %>%
+  mutate(percentile = percent_rank(wtcpue)) %>%
+  select(-wtcpue)
+
+##### PAUSED HERE #####
+
+  # pivot_wider(names_from = year, values_from = wtcpue, names_prefix = "yr") %>%
+  # mutate(
+  #   yrs1993_yr90 = yr1993 - yr1990,
+  #   yrs1996_yr93 = yr1996 - yr1993,
+  #   yrs1999_yr96 = yr1999 - yr1996,
+  #   yrs2003_yr99 = yr2003 - yr1999,
+  #   yrs2005_yr03 = yr2005 - yr2003,
+  #   yrs2007_yr05 = yr2007 - yr2005,
+  #   yrs2009_yr07 = yr2009 - yr2007,
+  #   yrs2011_yr09 = yr2011 - yr2009,
+  #   yrs2013_yr11 = yr2013 - yr2011,
+  #   yrs2015_yr13 = yr2015 - yr2013,
+  #   yrs2017_yr15 = yr2017 - yr2015,
+  #   yrs2019_yr17 = yr2019 - yr2017,
+  #   yrs2021_yr19 = yr2021 - yr2019,
+  #   yrs2023_yr21 = yr2023 - yr2021
+  # ) %>%
+  # ungroup() %>%
+  # select(region, spp, yrs1993_90:yrs2023_21) %>%
+  # mutate(total_change_wtcpue = yrs1993_90 + yrs1996_93 + yrs1999_96 + yrs2003_99 + yrs2005_03 + yrs2007_05 +
+  #                                  yrs2009_07 + yrs2011_09 + yrs2013_11 + yrs2015_13 + yrs2017_15 + yrs2019_17 +
+  #                                  yrs2021_19 + yrs2023_21) # this is a separate table
 
 ## Add presence absence (1/0), actual biomass, percentile for just that year
 
