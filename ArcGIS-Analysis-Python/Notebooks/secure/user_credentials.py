00ee3622b1340b13af8ea06127384a0c6d341278
JFK <95776196+john-f-kennedy-noaa@users.noreply.github.com>
2026-06-26 11:58:54 -0400

    oops

--- ArcGIS-Analysis-Python/Notebooks/secure/user_credentials.py
+++ ArcGIS-Analysis-Python/Notebooks/secure/user_credentials.py
@@ -1,5 +1,5 @@
 def main():
-    credentials={"username" : "john.f.kennedy@noaa.gov", "password" : "#FSM20132014fsm$"}
+    credentials={"username" : "", "password" : ""}
     return credentials
 
 if __name__ == '__main__':
